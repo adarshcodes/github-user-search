e61fe16f7f90853c2127d3c99d1212d933489d07
Adarshcodes <adarsh.lrnr@gmail.com>
2022-02-23 23:39:29 +0530

    completed styling card

--- src/components/UserData.jsx
+++ src/components/UserData.jsx
@@ -4,16 +4,16 @@ import ProfilePicture from "../assets/images/favicon-32x32.png";
 export default function UserData() {
 	return (
 		<div className="card">
-			<img src={ProfilePicture} alt="avatar" card--profile-picture />
+			<div className="card--pp">
+				<img src={ProfilePicture} alt="pp" />
+			</div>
 			<div className="card--card-data">
 				<div className="userdata">
-					<div className="card--card-data__name">
-						<h1 className="card--card-data__user heading-primary">
-							The Octocat
-						</h1>
-						<h3 className="card--card-data__username">@Octocat</h3>
+					<div className="userdata__name">
+						<h1 className="userdata__user heading-primary">The Octocat</h1>
+						<h3 className="userdata__username">@Octocat</h3>
 					</div>
-					<p className="card--card-data__date">Joined 25 Jan 2011</p>
+					<p className="userdata__date">Joined 25 Jan 2011</p>
 				</div>
 
 				<div className="card--card-data__bio">
@@ -24,17 +24,17 @@ export default function UserData() {
 				<div className="card--card-data__repo-card">
 					<div className="repo">
 						<p className="text">Repos</p>
-						<h2 className="heading-secondary">8</h2>
+						<h2 className="heading-secondary repo-numbers">8</h2>
 					</div>
 
 					<div className="followers">
 						<p className="text">Followers</p>
-						<h2 className="heading-secondary">3938</h2>
+						<h2 className="heading-secondary repo-numbers">3938</h2>
 					</div>
 
 					<div className="following">
 						<p className="text">Following</p>
-						<h2 className="heading-secondary">9</h2>
+						<h2 className="heading-secondary repo-numbers">9</h2>
 					</div>
 				</div>
 
@@ -46,7 +46,7 @@ export default function UserData() {
 
 					<div className="socialicon">
 						<i class="fa-brands fa-twitter"></i>
-						<a href="https://twitter.com">Twitter</a>
+						<a href="not yet">Not available</a>
 					</div>
 
 					<div className="socialicon">
