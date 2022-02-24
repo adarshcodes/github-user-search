cb3576d4ea0dc8611e1236a1ae198d1d4bfe3915
Adarshcodes <adarsh.lrnr@gmail.com>
2022-02-24 07:50:07 +0530

    fetching data from api

--- src/App.js
+++ src/App.js
@@ -22,12 +22,46 @@ function App() {
 	}, []);
 	// End of switching dark-mode
 
+	// Fetching data from GitHub API
+
+	const [userdata, setUserdata] = React.useState();
+	const token = "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";
+
+	React.useEffect(() => {
+		function getUser(username) {
+			fetch(`https://api.github.com/users/${username}`, {
+				headers: {
+					Authorization: `token ${token}`,
+				},
+			})
+				.then((res) => res.json())
+				.then((data) => setUserdata(data));
+		}
+		getUser("octocat");
+	}, []);
+
+	console.log(userdata);
+	// End of fetching data
+
 	return (
 		<div className="app">
 			<div className="main-container">
 				<Navigation isDark={isDark} toggle={toggleTheme} />
 				<SearchBar />
-				<UserData />
+				<UserData
+					avatar={userdata.avatar_url}
+					name={userdata.name}
+					username={userdata.login}
+					createdAt={userdata.createdAt}
+					bio={userdata.bio}
+					repos={userdata.repos_url}
+					followers={userdata.followers}
+					following={userdata.following}
+					location={userdata.location}
+					twitter={userdata.twitter_username}
+					blog={userdata.blog}
+					work={userdata.company}
+				/>
 			</div>
 		</div>
 	);
